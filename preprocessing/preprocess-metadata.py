07b8113d22e84c37a2531b9d9d8a1b0f9dbc5089
Jason Payne <paynejason@gmail.com>
2023-01-26 13:59:19 -0500

    Added Blacklist to Preprocessing
    Added Robert's blacklist as well as a term of our own to the term blacklist, then ran the program to update the csv.

--- preprocessing/preprocess-metadata.py
+++ preprocessing/preprocess-metadata.py
@@ -4,8 +4,15 @@ import text2term
 def main():
 	df = pd.read_csv("../metadata/nhanes_variables.csv")
 	text_to_process = df["SAS Label"].values.tolist()
-	processed_text = text2term.preprocess_terms(text_to_process, "templates.txt")
+
+	processed_text = text2term.preprocess_terms(text_to_process, "templates.txt", \
+								blacklist_path="term_blacklist.txt", blacklist_char="-")
 	df["Processed Text"] = processed_text
+
+	bl_df = pd.read_csv("table_blacklist.csv")
+	for index, row in bl_df.iterrows():
+		df.loc[df['Table'] == row['tables'], "Processed Text"] = "-"
+
 	df.to_csv("nhanes_variables_processed.csv", index=False)
 
 if __name__ == '__main__':
