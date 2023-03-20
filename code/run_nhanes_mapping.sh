f16733dcb5f0191810cf0f97569fef3b9c70a055
Rafael Goncalves <rsgoncalves@gmx.com>
2023-03-20 16:39:13 -0400

    Update shell script to generate SemSQL tables
    and update requirements accordingly

--- code/run_nhanes_mapping.sh
+++ code/run_nhanes_mapping.sh
@@ -1,9 +1,6 @@
 alias pip=pip3.9
 alias python=python3.9
 
-[ ! -d "ontology-utilities" ] && git clone 'https://github.com/ccb-hms/ontology-utilities.git'
-(cd "ontology-utilities" || return
-pip install . )
 pip install -r requirements.txt
-python generate_ontology_tables.py
+python generate_semsql_ontology_tables.py
 python generate_ontology_mappings.py
