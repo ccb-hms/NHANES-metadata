2c1f2ec5d63bd91e874e979952f9649bb531bd95
Rafael Goncalves <rsgoncalves@gmx.com>
2023-04-04 10:40:13 -0400

    Added query to get ontology version. Made some functions private
    Restricted labels table to ontology classes

--- code/generate_semql_ontology_tables.py
+++ code/generate_semql_ontology_tables.py
@@ -3,7 +3,7 @@ import sqlite3
 import urllib.request
 import pandas as pd
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 # Overlap between readily available SemanticSQL (https://github.com/INCATools/semantic-sql) databases and the
 # ontologies planned for use with NHANES
@@ -27,9 +27,10 @@ def get_semsql_ontology_tables(ontology_url, ontology_name, tables_output_folder
     conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
     # Get the tables from the sqlite database
-    edges_df = get_edges_table(cursor)
-    entailed_edges_df = get_entailed_edges_table(cursor)
-    labels_df = get_labels_table(cursor)
+    edges_df = _get_edges_table(cursor)
+    entailed_edges_df = _get_entailed_edges_table(cursor)
+    labels_df = _get_labels_table(cursor)
+    onto_version = _get_ontology_version(cursor)
     cursor.close()
     conn.close()
     if save_tables:
@@ -41,25 +42,33 @@ def get_semsql_ontology_tables(ontology_url, ontology_name, tables_output_folder
         entailed_edges_df.to_csv(entailed_edges_csv_file, index=False)
         label_csv_file = os.path.join(tables_output_folder, ontology_name.lower() + "_labels.csv")
         labels_df.to_csv(label_csv_file, index=False)
-    return edges_df, entailed_edges_df, labels_df
+    return edges_df, entailed_edges_df, labels_df, onto_version
 
 
-def get_edges_table(cursor):
+def _get_ontology_version(cursor):
+    cursor.execute("SELECT `value` FROM statements WHERE predicate='owl:versionInfo'")
+    version = cursor.fetchall().pop()[0]
+    return version
+
+
+def _get_edges_table(cursor):
     cursor.execute("SELECT * FROM edge WHERE predicate='rdfs:subClassOf'")
     edge_columns = [x[0] for x in cursor.description]
     edge_data = cursor.fetchall()
     return pd.DataFrame(edge_data, columns=edge_columns)
 
 
-def get_entailed_edges_table(cursor):
+def _get_entailed_edges_table(cursor):
     cursor.execute("SELECT * FROM entailed_edge WHERE predicate='rdfs:subClassOf'")
     entailed_edge_columns = [x[0] for x in cursor.description]
     entailed_edge_data = cursor.fetchall()
     return pd.DataFrame(entailed_edge_data, columns=entailed_edge_columns)
 
 
-def get_labels_table(cursor):
-    cursor.execute("SELECT * FROM statements WHERE predicate='rdfs:label'")  # Get rdfs:label statements
+def _get_labels_table(cursor):
+    # Get rdfs:label statements for ontology classes
+    cursor.execute("SELECT * FROM statements WHERE predicate='rdfs:label' AND subject IN "
+                   "(SELECT subject FROM statements WHERE predicate='rdf:type' AND object='owl:Class')")
     labels_columns = [x[0] for x in cursor.description]
     labels_data = cursor.fetchall()
     labels_df = pd.DataFrame(labels_data, columns=labels_columns)
