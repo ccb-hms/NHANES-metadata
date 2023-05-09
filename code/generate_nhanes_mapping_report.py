247c73604f035aca0f1728d0ad92b44d09a4ac7b
Rafael Goncalves <rsgoncalves@gmx.com>
2023-05-09 09:42:11 -0400

    Add code to generate mapping counts & merge them with ontology labels table

---
+++ code/generate_nhanes_mapping_report.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from generate_mapping_report import get_mapping_counts_to_ontologies
+
+
+if __name__ == "__main__":
+    mapping_counts_df = get_mapping_counts_to_ontologies(
+        mappings_df=pd.read_csv("../ontology-mappings/nhanes_variables_mappings.tsv", sep="\t"),
+        ontologies_df=pd.read_csv("resources/ontologies.csv"),
+        source_term_id_col="Variable",
+        source_term_secondary_id_col="Table")
+    mapping_counts_df.to_csv("nhanes_variables_mapping_counts.tsv", sep="\t", index=False)
+    mapping_counts_df = mapping_counts_df.drop(columns=["Ontology"])
+
+    labels_file = "../ontology-tables/ontology_labels.tsv"
+    labels_df = pd.read_csv(labels_file, sep="\t")
+
+    # Merge the counts table with the labels table on the "IRI" column
+    merged_df = pd.merge(labels_df, mapping_counts_df, on="IRI")
+    merged_df = merged_df.drop_duplicates()
+    merged_df.to_csv(labels_file, sep="\t", index=False)
--- code/run_nhanes_utilities.sh
+++ code/run_nhanes_utilities.sh
@@ -2,5 +2,7 @@ alias pip=pip3.9
 alias python=python3.9
 
 pip install -r requirements.txt
+
 python generate_semsql_ontology_tables.py
 python generate_ontology_mappings.py
+python generate_nhanes_mapping_report.py
