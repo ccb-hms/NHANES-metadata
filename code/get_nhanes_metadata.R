b87a2e7608454b45a7717f51f21af23b8879d8a6
Rafael Goncalves <rsgoncalves@gmx.com>
2023-10-30 10:19:56 -0400

    Modify code to obtain P tables metadata
    Use nhanesA from GitHub repo

--- code/get_nhanes_metadata.R
+++ code/get_nhanes_metadata.R
@@ -1,48 +1,83 @@
-install.packages("../lib/nhanesA_0.7.9.tar.gz", repos = NULL, type="source")
+install.packages("devtools")
+devtools::install_github("cjendres1/nhanes")
 library(nhanesA)
 library(progress)
+
+nhanes_tables_metadata_column_names <- c("Table", "TableName", "BeginYear", "EndYear", "DataGroup", 
+                                         "UseConstraints", "DocFile", "DataFile", "DatePublished")
 
 # Get metadata about NHANES tables from all survey years and data groups.
 get_tables_metadata <- function() {
   print("Extracting tables metadata...")
-  data_groups <- c("DEMOGRAPHICS", "DIETARY", "EXAMINATION", "LABORATORY", "QUESTIONNAIRE")
-  survey_years <- c(1999, 2001, 2003, 2005, 2007, 2009, 2011, 2013, 2015, 2017)
-  all_tables <- data.frame(matrix(ncol=6, nrow = 0))
+  data_groups <- c("Demographics", "Dietary", "Examination", "Laboratory", "Questionnaire")
+  survey_years <- c(1999, 2001, 2003, 2005, 2007, 2009, 2011, 2013, 2015, 2017, "P")
+  all_tables <- data.frame(matrix(ncol=9, nrow = 0))
+  pb <- progress_bar$new(total = length(survey_years))
   for (year in survey_years) {
+    pb$tick()
     for (data_group in data_groups) {
       tables_year <- get_tables(data_group=data_group, year=year)
       all_tables <- rbind(all_tables, tables_year)
     }
   }
-  colnames(all_tables) <-c("Table", "TableName", "BeginYear", "EndYear", "DataGroup", "UseConstraints", "DocFile", "DataFile", "DatePublished")
+  colnames(all_tables) <- nhanes_tables_metadata_column_names
   write.table(all_tables, file=paste(output_folder, "nhanes_tables.tsv", sep=""),
               row.names=FALSE, sep="\t", na="")
   return(all_tables)
 }
 
 get_tables <- function(data_group, year) {
-  tables <- nhanesTables(data_group=data_group, year=year, details=TRUE, includerdc=TRUE)
-  if(year=='P') {
-    # Pre-pandemic (2019-) tables have different header names that need renaming
-    colnames(tables)[colnames(tables)=="Data.File.Name"] = "Data.File.Description"
-    colnames(tables)[colnames(tables)=="Doc.File"] = "Data.File.Name"
-  }
-  tables$DocFile <- ""
-  tables$DataFile <- ""
-  tables$DatePublished <- ""
+  tables <- nhanesTables(data_group=toupper(data_group), year=year, details=TRUE, includerdc=TRUE)
+  tables_metadata <- data.frame(matrix(ncol=9, nrow = nrow(tables)))
+  colnames(tables_metadata) <- nhanes_tables_metadata_column_names
   for (i in 1:nrow(tables)) {
-    table_name <- tables$Data.File.Name[i]
-    search_pattern <- paste("\\b", table_name, "\\b", sep="")
-    table_details <- nhanesSearchTableNames(search_pattern, includerdc=TRUE, details=TRUE)
-    if(!is.null(table_details)) {
-      tables$DocFile[i] <- paste0(nhanesA:::nhanesURL, table_details$Years, "/", 
-                                  sapply(strsplit(table_name, " "), function(x) x[[1]]), ".htm")
-      tables$DataFile[i] <- paste0(nhanesA:::nhanesURL, table_details$Years, "/", 
-                                   sapply(strsplit(table_name, " "), function(x) x[[1]]), ".XPT")
-      tables$DatePublished[i] <- table_details[["Date.Published"]]
+    # Get the (unique) table identifier
+    table_id <- tables$Data.File.Name[i]
+    if(year=="P") {
+      table_id <- sub(" Doc", "", tables$Doc.File[i])
+    }
+    tables_metadata$Table[i] <- table_id
+    
+    # Get the table name (aka description)
+    table_name <- ""
+    if(year == "P") {
+      table_name <- tables$Data.File.Name[i]
+    } else {
+      table_name <- tables$Data.File.Description[i]
+    }
+    tables_metadata$TableName[i] <- table_name
+    
+    # Get the start and end years of the survey
+    if(year=="P") {
+      survey_years <- strsplit(tables$Years[i], "-")[[1]]
+      tables_metadata$BeginYear[i] <- survey_years[1]
+      tables_metadata$EndYear[i] <- survey_years[2]
+      url_years <- "2017-2018"
+    } else {
+      tables_metadata$BeginYear[i] <- tables$Begin.Year[i]
+      tables_metadata$EndYear[i] <- tables$EndYear[i]
+      url_years <- paste0(tables_metadata$BeginYear[i], "-", tables_metadata$EndYear[i])
+    }
+    tables_metadata$DataGroup[i] <- data_group
+    if(year != "P") {
+      tables_metadata$UseConstraints[i] <- tables$Use.Constraints[i]
+    }
+    # TODO: URLs for limited-access tables are currently missing the /limited_access/ bit
+    tables_metadata$DocFile[i] <- paste0(nhanesA:::nhanesURL, url_years, "/", 
+                                         sapply(strsplit(table_id, " "), function(x) x[[1]]), ".htm")
+    tables_metadata$DataFile[i] <- paste0(nhanesA:::nhanesURL, url_years, "/", 
+                                          sapply(strsplit(table_id, " "), function(x) x[[1]]), ".XPT")
+    if(year == "P") {
+      tables_metadata$DatePublished[i] <- tables$Date.Published[i]
+    } else {
+      search_pattern <- paste0("\\b", table_id, "\\b")
+      table_details <- nhanesSearchTableNames(search_pattern, includerdc=TRUE, details=TRUE, includewithdrawn=TRUE)
+      if(!is.null(table_details)) {
+        tables_metadata$DatePublished[i] <- table_details[["Date.Published"]]
+      }
     }
   }
-  return(tables)
+  return(tables_metadata)
 }
 
 # Get metadata about all variables in the given collection of NHANES tables.
@@ -96,67 +131,81 @@ get_variables_in_table <- function(table_name, nhanes_data_group) {
   tryCatch({
     table_codebooks <- nhanesCodebook(nh_table=table_name)
   },
-  error=function(e) {
-    log_missing_codebook("Error", conditionMessage(e), table_name=table_name, variable_name="")
+  error=function(msg) {
+    log_missing_codebook("Error in nhanesA::nhanesCodebook()", conditionMessage(msg), table_name=table_name, variable_name="")
+  }, warning=function(msg) {
+    log_missing_codebook("Warning in nhanesA::nhanesCodebook()", conditionMessage(msg), table_name=table_name, variable_name="")
   })
 
   # Get all variables in the specified table
   # Note: the data groups in the data frame returned by nhanesTables() function
   #   are in lower case, but the nhanesTableVars() function argument `data_group`
   #   is case-sensitive and only accepts upper-case data group names
-  table_variables <- nhanesTableVars(data_group=toupper(nhanes_data_group), 
-                                     nh_table=table_name, details=TRUE, 
-                                     nchar=1024, namesonly=FALSE)
-  
-  # For each variable in this table get its details
-  if(length(table_variables) > 0) {
-    for (variable in 1:nrow(table_variables)) {
-      variable_name <- table_variables[variable, 'Variable.Name']
-      use_constraints <- table_variables[variable, 'Use.Constraints']
-      tryCatch({
-        variable_details <- table_codebooks[variable_name][[1]]
-        if (!is.null(variable_details)) {
-          label <- clean(variable_details['SAS Label:'][[1]])
-          text <- clean(variable_details['English Text:'][[1]])
-          targets <- ""
-          for (i in seq_along(variable_details)) {
-            if ('Target:' %in% names(variable_details[i])) {
-              target <- clean(variable_details[[i]])
-              if (targets == "") {
-                targets <- target
-              } else {
-                targets <- paste(targets, target, sep = " *AND* ")
+  tryCatch({
+    table_variables <- nhanesTableVars(data_group=toupper(nhanes_data_group), 
+                                       nh_table=table_name, details=TRUE, 
+                                       nchar=1024, namesonly=FALSE)
+    # For each variable in this table get its details
+    if(length(table_variables) > 0) {
+      for (variable in 1:nrow(table_variables)) {
+        variable_name <- table_variables[variable, 'Variable.Name']
+        use_constraints <- table_variables[variable, 'Use.Constraints']
+        tryCatch({
+          variable_details <- table_codebooks[variable_name][[1]]
+          if (!is.null(variable_details)) {
+            label <- clean(variable_details['SAS Label:'][[1]])
+            text <- clean(variable_details['English Text:'][[1]])
+            targets <- ""
+            for (i in seq_along(variable_details)) {
+              if ('Target:' %in% names(variable_details[i])) {
+                target <- clean(variable_details[[i]])
+                if (targets == "") {
+                  targets <- target
+                } else {
+                  targets <- paste(targets, target, sep = " *AND* ")
+                }
               }
             }
-          }
-          variable_details_vector <- c(variable_name, table_name, label, text, targets, use_constraints)
-          all_variables[nrow(all_variables) + 1, ] <- variable_details_vector
-          variable_codebook <- variable_details[variable_name][[1]]
-          if (!is.na(variable_codebook) && variable_name != "SEQN" && variable_name != "SAMPLEID") {
-            if ("Value Description" %in% names(variable_codebook) && !is.null(variable_codebook[["Value Description"]])) {
-              variable_codebook[["Value Description"]] <- clean(variable_codebook[["Value Description"]])
+            variable_details_vector <- c(variable_name, table_name, label, text, targets, use_constraints)
+            all_variables[nrow(all_variables) + 1, ] <- variable_details_vector
+            variable_codebook <- variable_details[variable_name][[1]]
+            if (!is.null(variable_codebook)) {
+              if (variable_name != "SEQN" && variable_name != "SAMPLEID") {
+                if(length(variable_codebook) == 0) {
+                  print(paste("Variable codebook list for variable", variable_name, "in table", table_name, " is empty"))
+                }
+                if ("Value Description" %in% names(variable_codebook) && !is.null(variable_codebook[["Value Description"]])) {
+                  variable_codebook[["Value Description"]] <- clean(variable_codebook[["Value Description"]])
+                } else {
+                  variable_codebook[["Value Description"]] <- ""
+                }
+                names(variable_codebook)[names(variable_codebook) == 'Value Description'] <- 'ValueDescription'
+                variable_codebook <- cbind('Table'=table_name, variable_codebook)
+                variable_codebook <- cbind('Variable'=variable_name, variable_codebook)
+                all_variable_codebooks <- rbind(all_variable_codebooks, variable_codebook)
+              }
             } else {
-              variable_codebook[["Value Description"]] <- ""
+              log_missing_codebook("Warning", "Null codebook for this variable", table_name = table_name, variable_name = variable_name)
             }
-            names(variable_codebook)[names(variable_codebook) == 'Value Description'] <- 'ValueDescription'
-            variable_codebook <- cbind('Table'=table_name, variable_codebook)
-            variable_codebook <- cbind('Variable'=variable_name, variable_codebook)
-            all_variable_codebooks <- rbind(all_variable_codebooks, variable_codebook)
+          } else {
+            if (variable_name != "SEQN" && variable_name != "SAMPLEID") {
+              log_missing_codebook("Warning", "nhanesA::nhanesCodebook() does not return a codebook for this variable", table_name = table_name, variable_name = variable_name)
+            }
           }
-        } else {
-          if (variable_name != "SEQN" && variable_name != "SAMPLEID") {
-            log_missing_codebook("Warning", "Codebook does not exist for this variable", table_name = table_name, variable_name = variable_name)
-          }
-        }
-      }, error=function(e) {
-        log_missing_codebook("ERROR", conditionMessage(e), table_name=table_name, variable_name=variable_name)
-      }, warning=function(w) {
-        log_missing_codebook("WARNING", conditionMessage(w), table_name=table_name, variable_name=variable_name)
-      })
+        }, error=function(e) {
+          log_missing_codebook("ERROR", conditionMessage(e), table_name=table_name, variable_name=variable_name)
+        }, warning=function(w) {
+          log_missing_codebook("WARNING", conditionMessage(w), table_name=table_name, variable_name=variable_name)
+        })
+      }
+    } else {
+      print(paste("Warning: nhanesA::nhanesTableVars() returns 0 variables in table ", table_name))
     }
-  } else {
-    print(paste("Warning: nhanesA::nhanesTableVars() returns 0 variables in table ", table_name))
-  } 
+  }, error=function(msg) {
+    print(paste("Error in nhanesA::nhanesTableVars():", conditionMessage(msg)))
+  }, warning=function(msg) {
+    print(paste("Warning in nhanesA::nhanesTableVars():", conditionMessage(msg)))
+  })
   return(list(all_variables, all_variable_codebooks))
 }
 
