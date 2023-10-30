467e7d6d4ad5b126621163c0bf2603c7fe6a4b23
Rafael Goncalves <rsgoncalves@gmx.com>
2023-10-30 10:25:20 -0400

    Formulate filepaths in OS-independent way

--- code/build_database.py
+++ code/build_database.py
@@ -4,7 +4,7 @@ import pandas as pd
 import tarfile
 import sqlite3
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 ONTOLOGY_MAPPINGS_TABLE = 'ontology_mappings'
@@ -13,7 +13,7 @@ ONTOLOGY_MAPPINGS_TABLE = 'ontology_mappings'
 def build_database(database_name):
     Path(database_name).touch()
     db_connection = sqlite3.connect(database_name)
-    ontology_tables_folder = '../ontology-tables/'
+    ontology_tables_folder = os.path.join("..", "ontology-tables")
 
     # Import the edges and database cross-references tables
     ontology_edges_table = "ontology_edges"
@@ -22,30 +22,36 @@ def build_database(database_name):
     ontology_synonyms_table = "ontology_synonyms"
     ontology_tables_columns = "Subject TEXT,Object TEXT,Ontology TEXT"
 
-    import_table_to_db(db_connection, table_file=ontology_tables_folder + ontology_edges_table + '.tsv',
+    import_table_to_db(db_connection, table_file=os.path.join(ontology_tables_folder, ontology_edges_table + '.tsv'),
                        table_name=ontology_edges_table, table_columns=ontology_tables_columns)
 
-    import_table_to_db(db_connection, table_file=ontology_tables_folder + ontology_entailed_edges_table + '.tsv',
+    import_table_to_db(db_connection, table_file=os.path.join(ontology_tables_folder, ontology_entailed_edges_table + '.tsv'),
                        table_name=ontology_entailed_edges_table, table_columns=ontology_tables_columns)
 
-    import_table_to_db(db_connection, table_file=ontology_tables_folder + ontology_dbxrefs_table + '.tsv',
+    import_table_to_db(db_connection, table_file=os.path.join(ontology_tables_folder, ontology_dbxrefs_table + '.tsv'),
                        table_name=ontology_dbxrefs_table, table_columns=ontology_tables_columns)
 
-    import_table_to_db(db_connection, table_file=ontology_tables_folder + ontology_synonyms_table + '.tsv',
+    import_table_to_db(db_connection, table_file=os.path.join(ontology_tables_folder, ontology_synonyms_table + '.tsv'),
                        table_name=ontology_synonyms_table, table_columns=ontology_tables_columns)
 
     # Import the labels table
     term_labels_table_name = "ontology_labels"
     term_labels_table_columns = "Subject TEXT,Object TEXT,IRI TEXT,DiseaseLocation TEXT,Ontology TEXT," \
                                 "Direct INT,Inherited INT"
-    import_table_to_db(db_connection, table_file=ontology_tables_folder + term_labels_table_name + '.tsv',
+    import_table_to_db(db_connection, table_file=os.path.join(ontology_tables_folder, term_labels_table_name + '.tsv'),
                        table_name=term_labels_table_name, table_columns=term_labels_table_columns)
 
     # Import the ontology mappings table
     mappings_table_columns = "Variable TEXT,`Table` TEXT,SourceTermID TEXT,SourceTerm TEXT,MappedTermLabel TEXT," \
                              "MappedTermCURIE TEXT,MappedTermIRI TEXT,MappingScore REAL,Tags TEXT,Ontology TEXT"
-    import_table_to_db(db_connection, table_file="../ontology-mappings/nhanes_variables_mappings.tsv",
+    import_table_to_db(db_connection,
+                       table_file=os.path.join("..", "ontology-mappings", "nhanes_variables_mappings.tsv"),
                        table_name=ONTOLOGY_MAPPINGS_TABLE, table_columns=mappings_table_columns)
+
+    metadata_columns = "Variable TEXT,`Table` TEXT,SASLabel TEXT,EnglishText TEXT,Target TEXT,UseConstraints TEXT," \
+                       "ProcessedText TEXT,Tags TEXT,VariableID TEXT,OntologyMapped TEXT"
+    import_table_to_db(db_connection, table_file=os.path.join("..", "metadata", "nhanes_variables.tsv"),
+                       table_name="nhanes_variables_metadata", table_columns=metadata_columns)
     return db_connection
 
 
