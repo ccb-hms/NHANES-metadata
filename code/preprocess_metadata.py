29af72e89c68b9e7ad390301503d89b15f0d6e05
Rafael Goncalves <rsgoncalves@gmx.com>
2023-12-18 16:15:33 -0500

    Rename blocklists. Reapply table blocklist

--- code/preprocess_metadata.py
+++ code/preprocess_metadata.py
@@ -6,6 +6,14 @@ import os
 PROCESSED_TEXT_COL = "ProcessedText"
 PHENOTYPE_COL = "IsPhenotype"
 
+# The "blocklist_table" file contains non-phenotype variables specified by their precise identifiers
+BLOCKLIST_TABLE = "resources/blocklist_table.csv"
+
+# The "blocklist_regexps" file contains a list of regular expressions that are applied to the labels of variables, and
+# where matches occur, the corresponding variables are marked as non-phenotypes
+BLOCKLIST_REGEXPS = "resources/blocklist_regexps.txt"
+
+
 def preprocess(input_file, column_to_process, save_processed_table=False, input_file_col_separator=","):
     print("Preprocessing metadata table...")
     df = pd.read_csv(input_file, sep=input_file_col_separator, lineterminator="\n")
@@ -14,7 +22,7 @@ def preprocess(input_file, column_to_process, save_processed_table=False, input_
         temp_file.write('\n'.join(str(item) for item in text_to_process))
 
     processed_text = text2term.preprocess_tagged_terms(file_path="temp.txt", template_path="resources/templates.txt",
-                                                       blocklist_path="resources/term_blocklist.txt",
+                                                       blocklist_path=BLOCKLIST_REGEXPS,
                                                        blocklist_char="-")
     os.remove("temp.txt")
     df[PROCESSED_TEXT_COL] = ""
@@ -23,7 +31,11 @@ def preprocess(input_file, column_to_process, save_processed_table=False, input_
         df.loc[df[column_to_process] == term.get_original_term(), PROCESSED_TEXT_COL] = term.get_term()
         tags = ','.join(term.get_tags())
         df.loc[df[column_to_process] == term.get_original_term(), "Tags"] = tags
-    
+
+    bl_df = pd.read_csv(BLOCKLIST_TABLE)
+    for index, row in bl_df.iterrows():
+        df[PROCESSED_TEXT_COL] = np.where((df["Variable"] == row["names"]) & (df["Table"] == row["tables"]),
+                                          "-", df[PROCESSED_TEXT_COL])
     df = mark_phenotpyes(df)
     if save_processed_table:
         df.to_csv(input_file, sep="\t", index=False, mode="w")
